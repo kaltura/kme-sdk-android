a096a85d540f72e7d996c6246174561d78d5185c
Serhii Slobodianiuk <serhii.slobodianiuk@kaltura.com>
2020-10-07 12:54:07 +0300

    Added the UserCompanies screen Modified Koin implementation

--- src/main/java/com/kme/kaltura/kmesdk/KME.kt
+++ src/main/java/com/kme/kaltura/kmesdk/KME.kt
@@ -1,6 +1,8 @@
 package com.kme.kaltura.kmesdk
 
+import android.content.Context
 import com.kme.kaltura.kmesdk.di.KmeKoinComponent
+import com.kme.kaltura.kmesdk.di.KmeKoinContext
 import com.kme.kaltura.kmesdk.rest.KmeApiException
 import com.kme.kaltura.kmesdk.rest.controller.IKmeMetadataController
 import com.kme.kaltura.kmesdk.rest.controller.IKmeRoomController
@@ -30,10 +32,13 @@ class KME : KmeKoinComponent {
     }
 
     fun initSDK(
+        context: Context,
         success: () -> Unit,
         error: (exception: KmeApiException) -> Unit
     ) {
         if (isSDKInitialized) error(Exception("SDK already initialized!"))
+
+        KmeKoinContext.init(context)
 
         metadataController.fetchMetadata(success = {
             isSDKInitialized = true
@@ -42,5 +47,7 @@ class KME : KmeKoinComponent {
             error(it)
         })
     }
+
+    fun getFilesUrl() = metadataController.getMetadata()?.filesUrl
 
 }
