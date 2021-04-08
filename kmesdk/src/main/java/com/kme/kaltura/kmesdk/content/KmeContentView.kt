3ea484b068f4240dca82925ef7b2cd487d085326
Serhii Slobodianiuk <serhii.slobodianiuk@kaltura.com>
2021-04-08 18:02:49 +0300

    KMEMA-271 [Room] - fixed SurfaceView overlap issues

--- kmesdk/src/main/java/com/kme/kaltura/kmesdk/content/KmeContentView.kt
+++ kmesdk/src/main/java/com/kme/kaltura/kmesdk/content/KmeContentView.kt
@@ -2,10 +2,9 @@ package com.kme.kaltura.kmesdk.content
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
 import com.kme.kaltura.kmesdk.di.KmeKoinComponent
 
-abstract class KmeContentView: Fragment(), KmeKoinComponent {
+abstract class KmeContentView: KmeOverlapFragment(), KmeKoinComponent {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- kmesdk/src/main/java/com/kme/kaltura/kmesdk/util/widget/KmeOverlapLayout.kt
+++ kmesdk/src/main/java/com/kme/kaltura/kmesdk/util/widget/KmeOverlapLayout.kt
@@ -3,10 +3,9 @@ package com.kme.kaltura.kmesdk.util.widget
 import android.content.Context
 import android.graphics.Rect
 import android.util.AttributeSet
-import android.view.GestureDetector
-import android.view.LayoutInflater
-import android.view.MotionEvent
+import android.view.*
 import android.widget.FrameLayout
+import androidx.core.view.forEach
 import com.kme.kaltura.kmesdk.databinding.LayoutOverlapViewBinding
 import com.kme.kaltura.kmesdk.getDisplayMetrics
 
