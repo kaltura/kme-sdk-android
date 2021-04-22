96c0813525eda52199cb29d663c8064b297a87fa
Serhii Slobodianiuk <serhii.slobodianiuk@kaltura.com>
2021-04-22 11:24:54 +0300

    added configuration handler

--- kmesdk/src/main/java/com/kme/kaltura/kmesdk/di/RestModule.kt
+++ kmesdk/src/main/java/com/kme/kaltura/kmesdk/di/RestModule.kt
@@ -2,7 +2,7 @@ package com.kme.kaltura.kmesdk.di
 
 import com.google.gson.GsonBuilder
 import com.kme.kaltura.kmesdk.BuildConfig
-import com.kme.kaltura.kmesdk.R
+import com.kme.kaltura.kmesdk.rest.DynamicRetrofit
 import com.kme.kaltura.kmesdk.rest.KmeCookieJar
 import com.kme.kaltura.kmesdk.rest.KmeTokenInterceptor
 import com.kme.kaltura.kmesdk.rest.adapter.*
@@ -15,8 +15,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 /**
@@ -30,7 +28,10 @@ val restModule = module {
             .registerTypeAdapter(Boolean::class.javaPrimitiveType, KmeIntToBooleanTypeAdapter())
             .registerTypeAdapter(Boolean::class.javaObjectType, KmeStringToBooleanTypeAdapter())
             .registerTypeAdapter(Boolean::class.javaPrimitiveType, KmeStringToBooleanTypeAdapter())
-            .registerTypeAdapter(KmeResponseData::class.javaObjectType, KmeResponseDataTypeAdapter())
+            .registerTypeAdapter(
+                KmeResponseData::class.javaObjectType,
+                KmeResponseDataTypeAdapter()
+            )
             .registerTypeAdapter(KmeWhiteboardPath::class.java, KmeWhiteboardPathTypeAdapter())
             .registerTypeAdapter(KmeIntegrations::class.java, KmeIntegrationsAdapter())
             .create()
@@ -55,40 +56,35 @@ val restModule = module {
             .build()
     }
 
+    single(named("Downloader")) {
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
     single {
         KmeCookieJar(get())
     }
 
     single {
-        Retrofit.Builder()
-            .baseUrl("https://${androidContext().getString(R.string.api_url)}/backend/")
-            .client(get())
-            .addConverterFactory(GsonConverterFactory.create(get()))
-            .build()
+        DynamicRetrofit(androidContext(), get())
     }
 
     single(
         named("Downloader")
     ) {
-        Retrofit.Builder()
-            .baseUrl("https://${androidContext().getString(R.string.api_url)}/backend/")
-            .client(
-                OkHttpClient.Builder()
-                    .connectTimeout(30, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
-                    .build()
-            )
-            .build()
+        DynamicRetrofit(androidContext(), get(named("Downloader")))
     }
 
-    single { get<Retrofit>().create(KmeSignInApiService::class.java) }
-    single { get<Retrofit>().create(KmeUserApiService::class.java) }
-    single { get<Retrofit>().create(KmeRoomApiService::class.java) }
-    single { get<Retrofit>().create(KmeRoomNotesApiService::class.java) }
-    single { get<Retrofit>().create(KmeRoomRecordingApiService::class.java) }
-    single { get<Retrofit>().create(KmeChatApiService::class.java) }
-    single { get<Retrofit>().create(KmeMetadataApiService::class.java) }
+    single { get<DynamicRetrofit>().create(KmeSignInApiService::class.java) }
+    single { get<DynamicRetrofit>().create(KmeUserApiService::class.java) }
+    single { get<DynamicRetrofit>().create(KmeRoomApiService::class.java) }
+    single { get<DynamicRetrofit>().create(KmeRoomNotesApiService::class.java) }
+    single { get<DynamicRetrofit>().create(KmeRoomRecordingApiService::class.java) }
+    single { get<DynamicRetrofit>().create(KmeChatApiService::class.java) }
+    single { get<DynamicRetrofit>().create(KmeMetadataApiService::class.java) }
 
-    single { get<Retrofit>(named("Downloader")).create(KmeFileLoaderApiService::class.java) }
+    single { get<DynamicRetrofit>(named("Downloader")).create(KmeFileLoaderApiService::class.java) }
 }
