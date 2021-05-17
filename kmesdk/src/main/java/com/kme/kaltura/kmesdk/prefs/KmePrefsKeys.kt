769699512963fd21f7cb58cf6c0dc77724fb0bda
Alexander Rubanskiy <rubanskiy.a@gmail.com>
2021-05-17 13:18:49 +0300

    KMEMA-331. [Change configuration] Application crashes after change configuration.

--- kmesdk/src/main/java/com/kme/kaltura/kmesdk/prefs/KmePrefsKeys.kt
+++ kmesdk/src/main/java/com/kme/kaltura/kmesdk/prefs/KmePrefsKeys.kt
@@ -8,5 +8,6 @@ object KmePrefsKeys {
     const val ACCESS_TOKEN: String = "ACCESS_TOKEN"
     const val CSRF_TOKEN: String = "CSRF_TOKEN"
     const val COOKIE: String = "COOKIE"
+    const val BASE_SERVER_URL: String = "BASE_SERVER_URL"
 
 }
