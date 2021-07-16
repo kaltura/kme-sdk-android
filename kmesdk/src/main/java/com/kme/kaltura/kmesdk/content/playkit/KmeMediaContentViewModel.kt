285bd61517a1809352da4fab4768e6dbe7fdb3b4
zura <zphosta@gmail.com>
2021-07-16 13:54:39 +0400

    fixed audio manager volume problem when video was a appeared KMEMA-347

--- kmesdk/src/main/java/com/kme/kaltura/kmesdk/content/playkit/KmeMediaContentViewModel.kt
+++ kmesdk/src/main/java/com/kme/kaltura/kmesdk/content/playkit/KmeMediaContentViewModel.kt
@@ -1,17 +1,20 @@
 package com.kme.kaltura.kmesdk.content.playkit
 
+import android.media.AudioManager
 import androidx.lifecycle.ViewModel
 import com.kme.kaltura.kmesdk.controller.IKmeUserController
 import com.kme.kaltura.kmesdk.controller.room.IKmeRoomController
 import com.kme.kaltura.kmesdk.prefs.IKmePreferences
 import com.kme.kaltura.kmesdk.prefs.KmePrefsKeys
 import com.kme.kaltura.kmesdk.toNonNull
+import com.kme.kaltura.kmesdk.webrtc.audio.IKmeAudioManager
 import com.kme.kaltura.kmesdk.ws.message.type.KmeUserType
 import com.kme.kaltura.kmesdk.ws.message.type.permissions.KmePermissionValue
 
 class KmeMediaContentViewModel(
     private val roomController: IKmeRoomController,
     private val userController: IKmeUserController,
+    private val audioManager: IKmeAudioManager,
     private val prefs: IKmePreferences
 ) : ViewModel() {
 
@@ -27,4 +30,11 @@ class KmeMediaContentViewModel(
         return partnerId?.toInt() ?: 0
     }
 
+    fun videoVolumeIncrease(){
+        audioManager.adjustStreamVolumeRise()
+    }
+
+    fun videoVolumeDecrease(){
+        audioManager.adjustStreamVolumeLow()
+    }
 }
